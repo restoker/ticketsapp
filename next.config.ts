e3b5516713b94227492ae07e5e7e4b578c459379
restoker <milthon.doom@gmail.com>
2025-05-29 13:41:41 -0500

    hero page added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.cosmos.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nextuipro.nyc3.cdn.digitaloceanspaces.com',
+      },
+
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/(panel)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <h1>hola</h1>
+        </div>
+    );
+}
