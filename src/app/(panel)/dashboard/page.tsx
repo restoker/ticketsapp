c34e755ca8cba4bd8bdd4437daecd964ba822f19
restoker <milthon.doom@gmail.com>
2025-06-01 20:04:36 -0500

    dashboard ui added

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/server/auth";
 import { redirect } from "next/navigation";
+import Sidebard from "./ui/Sidebard";
 
 export default async function DashboardPage() {
     const session = await auth();
@@ -9,8 +10,16 @@ export default async function DashboardPage() {
     }
 
     return (
-        <div>
-            <h1>Wellcome to the dashboard</h1>
+        <div className="flex h-dvh w-full">
+            <Sidebard />
+            <div className="w-full flex-1 flex-col p-4">
+                <header className="flex items-center gap-3 rounded-medium border-small border-divider p-4">
+                    <h2 className="text-medium font-medium text-default-700">Overview</h2>
+                </header>
+                <main className="mt-4 h-full w-full overflow-visible">
+                    <div className="flex h-[90%] w-full flex-col gap-4 rounded-medium border-small border-divider" />
+                </main>
+            </div>
         </div>
     );
 }
